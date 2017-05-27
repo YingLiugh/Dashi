2bb1d81cd432295dea9c6399b4934b4e02379920
YingLiugh <ying.liu.18@dartmouth.edu>
2017-05-27 00:13:14 -0400

    added mapreduce

--- src/db/MongoDBConnection.java
+++ src/db/MongoDBConnection.java
@@ -85,6 +85,33 @@ public class MongoDBConnection implements DBConnection {
 
 	@Override
 	public JSONArray recommendRestaurants(String userId) {
+		try {
+			Set<String> visitedRestaurants = getVisitedRestaurants(userId);//step 1
+			Set<String> allCategories = new HashSet<>();// why hashSet? //step 2
+			for (String restaurant : visitedRestaurants) {
+				allCategories.addAll(getCategories(restaurant));
+			}
+			Set<String> allRestaurants = new HashSet<>();//step 3
+			for (String category : allCategories) {
+				Set<String> set = getBusinessId(category);
+				allRestaurants.addAll(set);
+			}
+			Set<JSONObject> diff = new HashSet<>();//step 4
+			int count = 0;
+			for (String businessId : allRestaurants) {
+				// Perform filtering
+				if (!visitedRestaurants.contains(businessId)) {
+					diff.add(getRestaurantsById(businessId, false));
+					count++;
+					if (count >= MAX_RECOMMENDED_RESTAURANTS) {
+						break;
+					}
+				}
+			}
+			return new JSONArray(diff);
+		} catch (Exception e) {
+			System.out.println(e.getMessage());
+		}
 		return null;
 	}
 
